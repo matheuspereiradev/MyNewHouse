1a92c8b96d6d76bd00b56b001b5197215fc9a064
matheus <matheuslima20111997@gmail.com>
2021-03-23 19:49:42 -0300

    verificação do token

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+import Erro from '../errors/AppError';
+
+interface TokenPayload{
+    email:string,
+    name:string,
+    permission:string,
+    iat:number,
+    exp:number,
+    sub:string
+}
+
+export default function ensureAuthenticated(request:Request,response:Response,next:NextFunction):void {
+    const authHeader = request.headers.authorization;
+    
+    if(!authHeader){
+        throw new Erro("JWT token not definided",1005,403);
+    }
+    //o jwt ira retornar Bearer ajsnijadinijs isso gera umarray com [type,token] mas type nao serve pra nada entao nem coloca ele
+    const [,token] = authHeader.split(' ');
+    const decode = verify(token,authConfig.jwt.secret);
+        
+    const {sub,email,name,permission} = decode as TokenPayload;
+
+    request.user = {
+        email:email,
+        permission:permission,
+        name:name,
+        id:sub
+    }
+
+    return next();
+
+}
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,11 @@
 import {Router,Request,Response} from 'express';
 import { routesSession } from './session.routes';
 import { routesUser } from './user.routes';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
 const routes = Router();
 
-routes.get('/healthz',(req:Request,res:Response)=>{res.json({"status":"running"})});
+routes.get('/healthz',ensureAuthenticated,(req:Request,res:Response)=>{res.json({"status":"running"})});
 
 routes.use('/user',routesUser);
 routes.use('/session',routesSession);
