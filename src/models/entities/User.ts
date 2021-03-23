9d2ad0aabaad00c6d0f95a351cb7fa0c3beadd35
matheus <matheuslima20111997@gmail.com>
2021-03-23 18:47:19 -0300

    entidade

---
+++ src/models/entities/User.ts
@@ -0,0 +1,76 @@
+import {Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn} from "typeorm"
+import {v4 as uuid} from 'uuid'
+import { City } from "./City";
+
+@Entity("tbgusuario")
+class User{
+
+    @PrimaryColumn()
+    readonly id:string;
+
+    @Column()
+    name:string;
+
+    @Column()
+    email:string;
+
+    @Column({name:"birth_date"})
+    birthDate:Date;
+
+    @Column()
+    password:string;
+
+    @Column()
+    cpf:string;
+
+    @Column()
+    cnpj:string;
+
+    @Column()
+    street:string;
+
+    @Column()
+    houseNumber:number;
+
+    @Column()
+    distrinct:string;
+
+    @Column()
+    complement:string;
+
+    @Column()
+    reference:string;
+
+    @Column()
+    income:number;
+
+    @Column({name:"phonenumber"})
+    phoneNumber:string;
+
+    @Column({name:"phonenumber_2"})
+    phoneNumber2:string;
+
+    @Column()
+    idCity:number;
+
+    @OneToOne(type=>City,city=>city.id)
+    @JoinColumn({name:"id_city"})
+    city:City
+
+    @Column({name:"is_broker"})
+    idPermission:boolean;
+
+    @CreateDateColumn()
+    created_at:Date;
+
+    @DeleteDateColumn()
+    deleted_at:Date;
+
+    constructor(){
+        if(!this.id){
+            this.id = uuid()
+        }
+    }
+}
+
+export{User};
