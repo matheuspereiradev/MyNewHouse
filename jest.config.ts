4655ad79785e60c9e42c25f19fdc478789b682e2
matheus <matheuslima20111997@gmail.com>
2021-03-25 18:22:16 -0300

    configurando jest para reconhecer decorators

--- jest.config.ts
+++ jest.config.ts
@@ -81,7 +81,11 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+      "shared/(.*)": "<rootDir>/src/shared/$1",
+      "config/(.*)": "<rootDir>/src/config/$1",
+      "modules/(.*)": "<rootDir>/src/modules/$1"
+    },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -173,7 +177,7 @@ export default {
   // A map from regular expressions to paths to transformers
   // transform: undefined,
 
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  // An array of regexp pattern strings that are matched against all source file baseUrls, matched files will skip transformation
   // transformIgnorePatterns: [
   //   "\\\\node_modules\\\\",
   //   "\\.pnp\\.[^\\\\]+$"
--- src/modules/user/services/CreateUserService.spec.ts
+++ src/modules/user/services/CreateUserService.spec.ts
@@ -1,13 +1,37 @@
-import {CreateUserService} from '@modules/user/services/CreateUserService';
+import "reflect-metadata"
+import {CreateUserService} from "../services/CreateUserService";
+import {FakeUserRepository} from "../infra/typeorm/repositories/fakes/FakeUserRepository"
 
-describe('Test Service CreateUser',()=>{
-    it('should be able to create a new user',()=>{
-        
-        // CreateUserService.e
-        // name, email, birthDate, password, cpf, cnpj, street, houseNumber, district, complement, reference, income, phoneNumber, phoneNumber2, idCity
+describe("Test Service CreateUser",()=>{
+    it("should be able to create a new user",async ()=>{
+        const repositoryFake = new FakeUserRepository();
+
+        const createUserService = new CreateUserService(repositoryFake);
+
+        const usr = await createUserService.execute({
+            name:"Joao Geraldo da Cruz",
+            email:"jo@email.com",
+            birthDate:new Date(),
+            password:"321",
+            street:"rua das orquideas",
+            cpf:"50989126013",
+            cnpj:"",
+            houseNumber:12,
+            district:"sao jose",
+            complement:"b",
+            reference:"na esquina",
+            income:900,
+            phoneNumber:"8892424740",
+            phoneNumber2:"92435678",
+            idCity:1
+        });
+
+        expect(usr).toHaveProperty("id");
+        expect(usr.cpf).toBe("50989126013")
+
     });
 
-    // it('not should be able to create a new user whith a wrong CPF',()=>{
+    // it("not should be able to create a new user whith a wrong CPF",()=>{
     //     expect(1+2).toBe(3)
     // });
 })
