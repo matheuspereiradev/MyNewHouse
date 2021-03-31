5d0c534128641b2358fd81ca17a7517f40bcadb5
matheus <matheuslima20111997@gmail.com>
2021-03-31 20:50:46 -0300

    caminho feliz

--- src/modules/user/services/CreateUserService.spec.ts
+++ src/modules/user/services/CreateUserService.spec.ts
@@ -4,6 +4,33 @@ import {FakeUserRepository} from "../infra/typeorm/repositories/fakes/FakeUserRe
 import Erro from '@shared/errors/AppError';
 
 describe("Test Service CreateUser",()=>{
+    it("should be able to create a new user",async ()=>{
+        const repositoryFake = new FakeUserRepository();
+
+        const createUserService = new CreateUserService(repositoryFake);
+
+        const usr = await createUserService.execute({
+            name:"Joao Geraldo da Cruz",
+            email:"jo@email.com",
+            birthDate:new Date(),
+            password:"321",
+            street:"rua das orquideas",
+            cpf:"50989126013",
+            cnpj:"",
+            houseNumber:12,
+            district:"sao jose",
+            complement:"b",
+            reference:"na esquina",
+            income:900,
+            phoneNumber:"8892424740",
+            phoneNumber2:"92435678",
+            idCity:1
+        });
+
+        expect(usr).toHaveProperty("id");
+        expect(usr.cpf).toBe("50989126013")
+
+    });
 
     it(" should not be able to create a new user whith a duplicated email",async()=>{
 
@@ -73,5 +100,7 @@ describe("Test Service CreateUser",()=>{
             idCity:1
         })).rejects.toBeInstanceOf(Erro)
     });
+
+    
 
 })
