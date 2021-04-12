c68724fc76c883483c8a7f8971cbff2c87cff627
matheus <matheuslima20111997@gmail.com>
2021-04-11 22:08:55 -0300

    repositorios fakes

---
+++ src/modules/property/infra/typeorm/repositories/fakes/PropertyRepositoryFake.ts
@@ -0,0 +1,42 @@
+import { getRepository, Repository } from 'typeorm';
+import IPropertyRepository from '@modules/property/IRepositories/IPropertyRepository';
+import { Property } from '@modules/property/infra/typeorm/entities/Property';
+import ICreatePropertyDTO from '@modules/property/dtos/ICreatePropertyDTO';
+
+class PropertyRepositoryFake implements IPropertyRepository{
+
+    private properties:Property[] = [];
+
+    public async findAll():Promise<Array<Property>>{
+        return this.properties;
+    }
+
+    public async findByID(id:string):Promise<Property>{
+        const result = this.properties.find(prop=>prop.id === id)
+        return result;
+    }
+
+    public async findByAdvertiser(userSlug:string):Promise<Array<Property>>{
+        /*const all = await this.ormRepository.();
+        return all;*/
+        return null
+    }
+
+    public async create(data:ICreatePropertyDTO):Promise<Property>{
+        
+        const property = new Property();
+
+        Object.assign(property,data);
+
+        this.properties.push(property);
+
+        return property;
+    }
+
+    public async countUserProperties(idUser:string):Promise<number>{
+        const count = 2;
+        return count;
+    }
+}
+
+export {PropertyRepositoryFake}
