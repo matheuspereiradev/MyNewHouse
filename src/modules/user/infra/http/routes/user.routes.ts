26821f2425910a377dc34208421f1ce172504cad
matheus <matheuslima20111997@gmail.com>
2021-04-18 17:43:13 -0300

    criando multer

--- src/modules/user/infra/http/routes/user.routes.ts
+++ src/modules/user/infra/http/routes/user.routes.ts
@@ -1,13 +1,16 @@
 import {Router,Request,Response} from 'express';
 import { UserController } from '@modules/user/controllers/UserController';
 import {PasswordController} from '@modules/user/controllers/PasswordController';
+import configUp from '@config/multer'
+import multer from 'multer';
 
 const routesUser = Router();
 
 const userController = new UserController();
+const mult = multer(configUp)
 
 routesUser.get('/',userController.show);
-routesUser.post('/',userController.create);
+routesUser.post('/',mult.single('avatar'),userController.create);
 
 
 export {routesUser};
