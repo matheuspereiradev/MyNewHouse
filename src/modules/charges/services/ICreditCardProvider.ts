cc0b4dc23186e41db3e1b48e77e053bc245dfbc5
matheus <matheuslima20111997@gmail.com>
2021-04-21 00:06:20 -0300

    estruturando provider

--- src/modules/charges/services/ICreditCardProvider.ts
+++ src/modules/charges/services/ICreditCardProvider.ts
@@ -1,17 +1,22 @@
 import IUserRepository from '@modules/user/IRepositories/IUserRepository';
 import Erro from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
+import ICreditCardProvider from '../IRepositories/ICreditCardRepository';
 
 @injectable()
 class GenerateCreaditCardCharge {
 
     constructor(
+        @inject('CreditCardProvider') 
+        private creditCardProvider:ICreditCardProvider,
         
         @inject('UserRepository') 
         private userRepository:IUserRepository
     ){}
 
     public async execute():Promise<any> {
+
+        await this.creditCardProvider.generate()
 
         return null;
     }
