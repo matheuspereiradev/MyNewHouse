1564053746896dafdae3c81c8f87153b76d39837
matheus <matheuslima20111997@gmail.com>
2021-04-21 00:21:40 -0300

    concluida arquitteura

---
+++ src/modules/charges/infra/providers/charges/implementation/CreditCardPagSeguro.ts
@@ -0,0 +1,12 @@
+import ICreditCardProvider from "../model/ICreditCardProvider";
+
+class CreditCardPagSeguro implements ICreditCardProvider{
+    
+    public async generate():Promise<any>{
+
+      
+          
+    }
+}
+
+export {CreditCardPagSeguro}
