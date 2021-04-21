b5d098fbaf4ec2cc641a78ca351dd4363d75964a
matheus <matheuslima20111997@gmail.com>
2021-04-21 00:24:14 -0300

    incluindo novo container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -13,7 +13,9 @@ import {GenerateChargeFake} from '@modules/billing/infra/providers/charges/fakes
 import IPlanRepository from '@modules/plans/IRepositories/IPlanRepository';
 import {PlanRepository} from '@modules/plans/infra/typeorm/repositories/PlanRepository'
 import IPropertyRepository from '@modules/property/IRepositories/IPropertyRepository';
-import {PropertyRepository} from '@modules/property/infra/typeorm/repositories/PropertyRepository'
+import {PropertyRepository} from '@modules/property/infra/typeorm/repositories/PropertyRepository';
+import ICreditCardProvider from '@modules/charges/infra/providers/charges/model/ICreditCardProvider';
+import {CreditCardPagSeguro} from '@modules/charges/infra/providers/charges/implementation/CreditCardPagSeguro'
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 container.registerSingleton<IHashProvider>('HashProvider', bcryptHashProvider);
@@ -21,6 +23,7 @@ container.registerSingleton<ISendMail>('SendMail',NodeMeiler);
 container.registerSingleton<IBillingRepository>('BillingRepository',BillingRepository);
 container.registerSingleton<IPlanRepository>('PlanRepository',PlanRepository);
 container.registerSingleton<IPropertyRepository>('PropertyRepository',PropertyRepository);
+container.registerSingleton<ICreditCardProvider>('CreditCardProvider',CreditCardPagSeguro);
 
 //TODO COLOCAR GERADOR CORRETO
 container.registerSingleton<IGenerateCharge>('GenerateRemoteCharge',GenerateChargeFake);
