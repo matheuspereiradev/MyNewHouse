b32ef1aa31d61e0f56df60e6b939847040cc7abc
matheus <matheuslima20111997@gmail.com>
2021-04-21 18:44:33 -0300

    crianod DTO

---
+++ src/modules/user/dtos/IUpdateUserDTO.ts
@@ -0,0 +1,22 @@
+export default interface IUpdateUserDTO{
+    id:string,
+    name:string, 
+    surname:string,
+    email:string,
+    birthDate:Date,
+    password:string,
+    cpf:string,
+    cnpj:string,
+    street:string,
+    houseNumber:number,
+    district:string, 
+    complement:string,
+    reference:string,
+    income:number
+    phoneNumber:string,
+    phoneNumber2:string,
+    idCity:number,
+    avatar:string,
+    gender:string,
+    slug:string
+}
